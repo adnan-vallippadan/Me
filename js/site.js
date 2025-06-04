ec18e05715b8583a261168d2a37a806ae8aafb7d
Adnan Vallippadan <adnanvp9@gmail.com>
2025-06-04 18:30:20 +0530

    First Commit
    First Commit

---
+++ js/site.js
@@ -0,0 +1,18 @@
+let site = {
+    initialize: function() {        
+        site.setCurrentYear();
+        site.initializeLoader();
+    },
+
+    initializeLoader: function() {
+        setTimeout(() => {
+            let element = document.getElementById("loading-animation");
+            element.style.display = 'none';
+        }, 800);
+    },
+
+    setCurrentYear: function() {
+        const year = new Date().getFullYear();
+        document.getElementById('year').textContent = year;
+    }
+}
