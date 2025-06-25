6a4e29398b6bfd79d4b1f0b44728099dd9692ecf
Adnan Vallippadan <adnanvp9@gmail.com>
2025-06-25 18:51:17 +0530

    hero block improvements

--- js/site.js
+++ js/site.js
@@ -1,18 +1,30 @@
 let site = {
-    initialize: function() {        
+    initialize: function () {
         site.setCurrentYear();
         site.initializeLoader();
+        site.initScroll();
+
+        window.addEventListener('scroll', site.initScroll);
     },
 
-    initializeLoader: function() {
+    initializeLoader: function () {
         setTimeout(() => {
             let element = document.getElementById("loading-animation");
             element.style.display = 'none';
         }, 800);
     },
 
-    setCurrentYear: function() {
+    setCurrentYear: function () {
         const year = new Date().getFullYear();
         document.getElementById('year').textContent = year;
+    },
+
+    initScroll: function () {
+        const myDiv = document.getElementById('nav-header');
+        if (window.scrollY > 0) {
+            myDiv.classList.add('bg-primary-900');
+        } else {
+            myDiv.classList.remove('bg-primary-900');
+        }
     }
 }
